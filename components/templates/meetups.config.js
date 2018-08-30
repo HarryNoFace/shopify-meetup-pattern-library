c517d0221be026d3a02583b7e7b190f119a0c5c7
Harry Davies <harry@noface.co.uk>
2018-08-30 11:16:42 +0100

    Desktop sites except single-event

--- components/templates/meetups.config.js
+++ components/templates/meetups.config.js
@@ -1,7 +1,19 @@
 "use strict";
 
+const faker = require("faker");
+const eventsCount = 6;
+const events = [];
+
+for (var i = 0; i < eventsCount; i++) {
+	events.push({
+		contentTitle: "Shopify " + faker.address.city()
+	});
+}
+
 module.exports = {
 	context: {
-		heroTitle: "Meetups"
+		eventsData: events,
+		heroTitle: "Meetups",
+		updateTitle: "Shopify Dropshipping: A complete guide"
 	}
 };
